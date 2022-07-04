2039641fc52b25f31d4a288939f9303bc8768aa2
andreas-klumpp <andreas.klumpp@thoughtworks.com>
2022-07-04 09:19:00 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MicroFrontend from './MicroFrontend';
+
+const {REACT_APP_MICROFRONTEND_1, REACT_APP_MICROFRONTEND_2} = process.env;
+
+function MicroFrontend1() {
+  return <MicroFrontend host={REACT_APP_MICROFRONTEND_1} name="microfrontend-1"/>
+}
+
+function MicroFrontend2() {
+  return <MicroFrontend host={REACT_APP_MICROFRONTEND_2} name="microfrontend-2"/>
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <iframe src="http://localhost:3001" width="600" height="900">
+          <p>Your browser does not support iframes.</p>
+        </iframe>
+
+        <iframe src="http://localhost:3002" width="600" height="900">
+          <p>Your browser does not support iframes.</p>
+        </iframe>
+        {/* <div className='container'>
+          <MicroFrontend1 />
+          <MicroFrontend2 />
+        </div> */}
       </header>
     </div>
   );
